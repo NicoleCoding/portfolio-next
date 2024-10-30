023ec8d815ebc4bbb2e637deeb41d29e01b07a78
NicoleCoding <nicole251097@gmail.com>
2024-10-30 22:13:31 +0200

    - Centered copyright text in footer - Downgraded React version - Implemented hamburger-react

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa";
 export default function Footer() {
     return (
         <footer className="pt-6 mt-5 bg-black text-white font-custom w-full">
-            <p className="p-2">&copy; 2024 NicoleCoding. All rights reserved.</p>
+            <p className="p-2 text-center">&copy; 2024 NicoleCoding. All rights reserved.</p>
             <div className="flex flex-row justify-center items-center gap-10">
                 <p className="font-black">Follow me on: </p><a href="https://www.linkedin.com/in/nicole-saucinitanu-9b95ba167/" className="flex items-center justify-center hover:text-teal-400 m-2 text-white"><FaLinkedin className="m-1 text-white" />LinkedIn</a>
                 <a href="https://github.com/NicoleCoding" className="flex items-center justify-center hover:text-teal-400 m-2 text-white"><FaGithub className="m-1 text-white" />GitHub</a>
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,16 @@
 import Link from "next/link";
+import { useState } from "react";
+import Hamburger from "hamburger-react";
 
 export default function Nav() {
+    const [isOpen, setOpen] = useState(false);
 
     return (
         <nav className="flex flex-col md:flex-row items-center font-custom">
-            <ul>
+            <div className="md:hidden">
+                <Hamburger toggled={isOpen} toggle={setOpen}/>
+            </div>
+            <ul className={`flex flex-col md:flex-row md:items-center justify-end transition-transform duration-300 ${isOpen ? "block" : "hidden"} md:flex`}>
                 <li className="mb-2 md:mb-0"><Link href="/">Home</Link></li>
                 <li className="mb-2 md:mb-0"><Link  href="/projects">Projects</Link></li>
                 <li className="mb-2 md:mb-0"><Link  href="/about">About</Link></li>
