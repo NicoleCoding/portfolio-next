62aa97ec6e5584c229b660ca6aa1109ff5aa73bc
NicoleCoding <nicole251097@gmail.com>
2024-11-06 15:31:40 +0200

    - Added speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }) {
      <main className="flex-grow pt-6 px-4 md:px-0 py-0">
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
      </main>
     <Footer />
     </div>
